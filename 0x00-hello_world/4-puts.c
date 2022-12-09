bc17d6765a76b83f611da939118da76d8748e07b
walesy4real <walesy4real@gmail.com>
2022-12-08 19:05:06 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,10 @@
  * Return: Always 0 (sucess)
  */
 
+
 int main(void)
 {
-	puts('\'Programming is like building a multilingual puzzle');
-	return (0);
-
-
+	puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
 }
 
