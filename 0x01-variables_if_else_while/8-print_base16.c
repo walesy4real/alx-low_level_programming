cc3c16fd19dc23704e4e0e7462487972ea6ea48c
walesy4real <walesy4real@gmail.com>
2022-12-10 11:07:43 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point prints
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
