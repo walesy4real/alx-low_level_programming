20decd8640cb653e6a3d6fef7962540348efb946
walesy4real <walesy4real@gmail.com>
2022-12-12 00:27:58 -0800

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,7 @@ int main(void)
 	int n;
 
 	for (n = 48; n < 58; n++)
-	{
-		putchar (n);
+	putchar (n);
 		if (n != 57)
 		{
 			putchar(',');
@@ -18,6 +17,7 @@ int main(void)
 		}
 
 	putchar('\n');
+
 	return (0);
-	}
+
 }
