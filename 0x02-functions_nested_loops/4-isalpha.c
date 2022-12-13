9e112728ea3cf1c7c64f6a74155d463c8d860368
walesy4real <walesy4real@gmail.com>
2022-12-13 12:27:12 -0800

    5-sign

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+int _isalpha(int d)
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
+
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - return 0 not lower while 1 for lower
+ *
+ * Return: Always 0 (success)
+ *
+*/
+int print_sign(int n)
+{
+	int test;
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('-');
+	}
+	return (test);
+}
