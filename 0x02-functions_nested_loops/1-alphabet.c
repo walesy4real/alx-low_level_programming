c5e5651a9e4ad9d3d56264a742e671e8230255f4
walesy4real <walesy4real@gmail.com>
2022-12-13 07:14:06 -0800

    good

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabets - entry point
+ *
+ * Return: 0
+*/
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
