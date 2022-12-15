7ea2ff66a5418ac5a34ddc1cfbf3af27088899ab
walesy4real <walesy4real@gmail.com>
2022-12-15 13:29:00 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,14 @@
  */
 
 void print_remaining_days(int month, int day, int year)
+
 {
 	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
+		if (month > 2 && day >= 60)
+
+		{
+
 		day++;
 	}
 	printf("day of the year: %d\n", day);
@@ -22,12 +27,13 @@ else
 {
 	if (month == 2 && day == 60)
 	{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
-	else
-	{ printf("day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
-	}
+else
+	{
+	 printf("day of the year: %d\n", day);
+	 printf("Remaining days: %d\n", 365 - day);
+
 }
-
-
+}
+}
